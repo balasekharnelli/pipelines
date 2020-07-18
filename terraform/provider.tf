5d0e3c28f91edff34a857132fa1a0a6d1f86c817
Nelli, Balasekhar <Balasekhar.Nelli@pearson.com>
2020-07-18 13:05:11 +0530

    Updated terraform pipeline-5

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   required_version = "~> 0.12.21"
-#   required_providers {
-#     aws = "~> 2.52"
-#   }
-# }
+terraform {
+  required_version = "~> 0.12.21"
+  required_providers {
+    aws = "~> 2.52"
+  }
+}
 
 provider "aws" {
   region  = var.aws_region
