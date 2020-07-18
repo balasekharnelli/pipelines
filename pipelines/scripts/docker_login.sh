6e8fd5df8f7c81e18a05954ecb25d67f138ea9ec
Nelli, Balasekhar <Balasekhar.Nelli@pearson.com>
2020-07-18 11:43:32 +0530

    Updated pipeline-2

--- pipelines/scripts/docker_login.sh
+++ pipelines/scripts/docker_login.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-ECR_URI = "784579436685.dkr.ecr.us-east-1.amazonaws.com"
-AWS_REGION = "us-east-1"
-
-echo "Docker logging into ECR"
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URI
+echo "Docker logging into ECR by $USER"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 784579436685.dkr.ecr.us-east-1.amazonaws.com
